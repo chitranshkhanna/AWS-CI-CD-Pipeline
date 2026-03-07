df3a6a814a95ed1fd64a2502a995f64d13e05853
Chitransh Khanna <chitranshkhannaa26@gmail.com>
2026-03-07 20:46:20 +0530

    Initial commit for AWS CI/CD Terraform project

---
+++ terraform/20-bastion/data.tf
@@ -0,0 +1,23 @@
+data "aws_ssm_parameter" "bastion_sg_id" {
+  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+}
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+}
+
+data "aws_ami" "ami_info" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+}
---
+++ terraform/20-bastion/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 6.28.0"
+    }
+  }
+  backend "s3" {
+    bucket = "chitransh-terraform-state-2026"
+    key    = "expense-dev-bastion"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
